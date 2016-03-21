06ecb351abf2511c4cfd0615942e2af62a128d1a
Sandra Bristiel <sandra.bristiel@tactfactory.com>
2016-03-21 16:04:50 +0100

    Add quote entities

---
+++ Groupe4/front/src/main/java/com/group4/front/entities/QuoteHome.java
@@ -0,0 +1,104 @@
+package com.group4.front.entities;
+
+public class QuoteHome extends Quote {
+
+    private String typeHome;
+    private int area;
+    private int numberRoom;
+    private int floor;
+    private int numberBathroom;
+    private boolean garage;
+    private int groundArea;
+    private int terraceArea;
+    private String typeHeating;
+    private String formula;
+    private String address;
+
+    public String getTypeHome() {
+        return typeHome;
+    }
+
+    public void setTypeHome(String typeHome) {
+        this.typeHome = typeHome;
+    }
+
+    public int getArea() {
+        return area;
+    }
+
+    public void setArea(int area) {
+        this.area = area;
+    }
+
+    public int getNumberRoom() {
+        return numberRoom;
+    }
+
+    public void setNumberRoom(int numberRoom) {
+        this.numberRoom = numberRoom;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public int getNumberBathroom() {
+        return numberBathroom;
+    }
+
+    public void setNumberBathroom(int numberBathroom) {
+        this.numberBathroom = numberBathroom;
+    }
+
+    public boolean isGarage() {
+        return garage;
+    }
+
+    public void setGarage(boolean garage) {
+        this.garage = garage;
+    }
+
+    public int getGroundArea() {
+        return groundArea;
+    }
+
+    public void setGroundArea(int groundArea) {
+        this.groundArea = groundArea;
+    }
+
+    public int getTerraceArea() {
+        return terraceArea;
+    }
+
+    public void setTerraceArea(int terraceArea) {
+        this.terraceArea = terraceArea;
+    }
+
+    public String getTypeHeating() {
+        return typeHeating;
+    }
+
+    public void setTypeHeating(String typeHeating) {
+        this.typeHeating = typeHeating;
+    }
+
+    public String getFormula() {
+        return formula;
+    }
+
+    public void setFormula(String formula) {
+        this.formula = formula;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
