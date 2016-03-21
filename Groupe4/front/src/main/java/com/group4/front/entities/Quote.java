b05da2a4b9f014f5ba5648887e53e18c6fbe897f
Sandra Bristiel <sandra.bristiel@tactfactory.com>
2016-03-21 16:04:50 +0100

    Add quote entities

---
+++ Groupe4/front/src/main/java/com/group4/front/entities/Quote.java
@@ -0,0 +1,65 @@
+package com.group4.front.entities;
+
+public class Quote {
+
+    /** The id. */
+    private int id;
+    /** The name. */
+    private String name;
+    /** The resume. */
+    private String resume;
+    /** Price. */
+    private float price;
+    /** Current step. */
+    private int step;
+    /** Login. */
+    private String login;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getResume() {
+        return resume;
+    }
+
+    public void setResume(String resume) {
+        this.resume = resume;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getStep() {
+        return step;
+    }
+
+    public void setStep(int step) {
+        this.step = step;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+}
