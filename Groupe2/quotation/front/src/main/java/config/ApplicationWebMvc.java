f9413ddd599771f13aed350093e6e40a7d24faab
Nicolas Rauflet <rauflet.nicolas@outlook.fr>
2016-03-22 15:50:39 +0100

    Close Archi

--- Groupe2/quotation/front/src/main/java/config/ApplicationWebMvc.java
+++ Groupe2/quotation/front/src/main/java/config/ApplicationWebMvc.java
@@ -1,14 +1,10 @@
 package config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@EnableWebMvc
 @Configuration
-@ComponentScan(basePackages = { "com.group.two.front.controller" })
 public class ApplicationWebMvc extends WebMvcConfigurerAdapter {
 
     @Override
