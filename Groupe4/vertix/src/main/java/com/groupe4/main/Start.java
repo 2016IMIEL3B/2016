417f8c70b3f4b47e511bae7e9cc7918f5e29c8ed
Charles Guérin-Rouard <charles.guerin.rouard@gmail.com>
2016-03-22 12:10:40 +0100

    Ajout de vertx web et configuration générale des routes

--- Groupe4/vertix/src/main/java/com/groupe4/main/Start.java
+++ Groupe4/vertix/src/main/java/com/groupe4/main/Start.java
@@ -1,9 +1,7 @@
 package com.groupe4.main;
 
 import io.vertx.core.Vertx;
-/**
- * Created by Mathilde on 21/03/2016.
- */
+
 public class Start {
 
     public static void main(String[] args) {
--- Groupe4/vertix/src/main/java/com/groupe4/main/VerticleService.java
+++ Groupe4/vertix/src/main/java/com/groupe4/main/VerticleService.java
@@ -1,19 +1,50 @@
 package com.groupe4.main;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Future;
-/**
- * Created by Mathilde on 21/03/2016.
- */
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
+
 public class VerticleService extends AbstractVerticle{
 
     @Override
-    public void start(Future<Void> startFuture) {
-        System.out.println("MyVerticle started!");
+    public void start() {
+        Router router = Router.router(vertx);
+
+        router.route().handler(BodyHandler.create());
+        router.route("/api/*").produces("application/json");
+        router.route("/api/*").handler(context -> {
+            context.response().headers().add(HttpHeaders.CONTENT_TYPE, "application/json");
+            context.response().headers().add("content-type", "text/html;charset=UTF-8");
+
+            context.response()
+                    // do not allow proxies to cache the data
+                    .putHeader("Cache-Control", "no-store, no-cache")
+                    // prevents Internet Explorer from MIME - sniffing a
+                    // response away from the declared content-type
+                    .putHeader("X-Content-Type-Options", "nosniff")
+                    // Strict HTTPS (for about ~6Months)
+                    .putHeader("Strict-Transport-Security", "max-age=" + 15768000)
+                    // IE8+ do not allow opening of attachments in the context
+                    // of this resource
+                    .putHeader("X-Download-Options", "noopen")
+                    // enable XSS for IE
+                    .putHeader("X-XSS-Protection", "1; mode=block")
+                    // deny frames
+                    .putHeader("X-FRAME-OPTIONS", "DENY")
+                    // Accept all
+                    .putHeader("Access-Control-Allow-Origin", "*");
+
+            context.next();
+        });
+
+        vertx.createHttpServer().requestHandler(router::accept).listen(1204);
+
+        System.out.println("Http server is running...");
     }
 
     @Override
-    public void stop(Future stopFuture) throws Exception {
-        System.out.println("MyVerticle stopped!");
+    public void stop() throws Exception {
+        System.out.println("Verticle service stopped!");
     }
 }
