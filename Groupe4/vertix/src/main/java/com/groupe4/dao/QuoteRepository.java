81c663fb1eee6fde51cb88d2efe5589a6737e815
jalips <alipsjuien@hotmail.fr>
2016-03-22 16:18:18 +0100

    add maven dependency

--- Groupe4/vertix/src/main/java/com/groupe4/dao/QuoteRepository.java
+++ Groupe4/vertix/src/main/java/com/groupe4/dao/QuoteRepository.java
@@ -1,8 +1,45 @@
 package com.groupe4.dao;
 
+import com.groupe4.entity.Quote;
+import io.vertx.core.json.JsonArray;
+import io.vertx.ext.sql.ResultSet;
+import io.vertx.ext.sql.SQLConnection;
+
+import java.util.List;
+
 /**
  * Created by Link on 22/03/2016.
  */
 public class QuoteRepository {
-    
+
+    private SQLConnection connection;
+
+    public QuoteRepository(SQLConnection connection) {
+        this.connection = connection;
+    }
+
+    /*
+    public List<Quote> getQuotes() {
+        List<Quote> listQuote;
+        connection.query("SELECT * FROM quote", res2 -> {
+            if (res2.succeeded()) {
+                ResultSet rs = res2.result();
+
+                List<String> columnNames = rs.getColumnNames();
+                List<JsonArray> results = rs.getResults();
+
+
+
+                for (JsonArray row: results) {
+                    Quote nQuote = new Quote();
+                    //listQuote.add(nQuote);
+
+                    //System.out.println("User "+row.getString(1));
+
+                }
+            }
+        });
+        return List<Quote> listQuote;
+    }
+    */
 }
