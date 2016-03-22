b3ebdef92d4186756708be0dab3fa3e4b4fe6b57
jalips <alipsjuien@hotmail.fr>
2016-03-22 14:22:58 +0100

    #11 : Package Connexion OK

--- Groupe4/vertix/src/main/java/com/groupe4/main/Start.java
+++ Groupe4/vertix/src/main/java/com/groupe4/main/Start.java
@@ -1,10 +1,7 @@
 package com.groupe4.main;
 
+import com.groupe4.connexion.Connexion;
 import io.vertx.core.Vertx;
-import io.vertx.core.json.JsonObject;
-import io.vertx.ext.asyncsql.AsyncSQLClient;
-import io.vertx.ext.asyncsql.MySQLClient;
-import io.vertx.ext.sql.SQLConnection;
 
 /**
  * Created by Mathilde on 21/03/2016.
@@ -17,25 +14,8 @@ public class Start {
 
         vertx.deployVerticle(new VerticleService());
 
-        JsonObject mySQLClientConfig = new JsonObject()
-                .put("host", "127.0.0.1")
-                .put("port", 3306)
-                .put("username", "root")
-                .put("password", "toto")
-                .put("database", "java_g4_ms");
-        AsyncSQLClient mySQLClient = MySQLClient.createShared(vertx, mySQLClientConfig);
+        Connexion connect_bdd = new Connexion(vertx);
 
-        mySQLClient.getConnection(res -> {
-
-            if (res.succeeded()) {
-                SQLConnection connection = res.result();
-                // Got a connection
-                System.out.println("Connexion OK");
-            } else {
-                // Failed to get connection - deal with it
-                System.out.println("Connexion NOK");
-            }
-        });
 
     }
 }
