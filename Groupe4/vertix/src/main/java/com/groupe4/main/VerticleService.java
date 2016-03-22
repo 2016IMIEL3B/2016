b9cb749282ee351d1d6c8ed30209457b375b2fbb
jalips <alipsjuien@hotmail.fr>
2016-03-22 14:09:43 +0100

    Connexion bdd OK

--- Groupe4/vertix/src/main/java/com/groupe4/main/VerticleService.java
+++ Groupe4/vertix/src/main/java/com/groupe4/main/VerticleService.java
@@ -2,6 +2,15 @@ package com.groupe4.main;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.asyncsql.AsyncSQLClient;
+import io.vertx.ext.asyncsql.MySQLClient;
+import io.vertx.ext.sql.SQLConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Created by Mathilde on 21/03/2016.
  */
