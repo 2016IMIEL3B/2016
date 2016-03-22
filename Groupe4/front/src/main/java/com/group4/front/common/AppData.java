9eb2fa7f5aa34f61ef722a39ed872f31db336b20
Charles Guérin-Rouard <charles.guerin.rouard@gmail.com>
2016-03-22 11:31:20 +0100

    Configuration sécurité Spring

---
+++ Groupe4/front/src/main/java/com/group4/front/common/AppData.java
@@ -0,0 +1,35 @@
+package com.group4.front.common;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(proxyMode = ScopedProxyMode.TARGET_CLASS, scopeName = "session")
+public class AppData {
+    /**
+     * User token.
+     */
+    private String token;
+
+    /**
+     * User id.
+     */
+    private Integer idUser;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Integer getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(Integer idUser) {
+        this.idUser = idUser;
+    }
+}
