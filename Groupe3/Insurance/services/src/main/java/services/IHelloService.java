d4fee37acc6f326094911cbc51c794277a766cd9
Miroufff <sylvain.davenel@laposte.net>
2016-03-23 12:14:47 +0100

    Connect mysql db

--- Groupe3/Insurance/services/src/main/java/services/IHelloService.java
+++
@@ -1,8 +0,0 @@
-package services;
-
-/**
- * Created by Nico on 09/02/2016.
- */
-public interface IHelloService {
-    public String hello();
-}
---
+++ Groupe3/Insurance/services/src/main/java/services/auth/AuthClientService.java
@@ -0,0 +1,29 @@
+package services.auth;
+
+import model.User;
+import services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * Created by Sylvain on 11/02/2016.
+ */
+@Component
+public class AuthClientService implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userService.findByLogin(username);
+        if (user != null) return user;
+
+        throw new UsernameNotFoundException("Username not found");
+    }
+}
