9e51848c5a949020780763000be7f7565ad67822
jalips <alipsjulien@hotmail.fr>
2016-04-19 11:12:22 +0200

    refs #31 : Modification fixtures

--- Groupe4/_docs/ScriptsSQL/AllRequest.sql
+++ Groupe4/_docs/ScriptsSQL/AllRequest.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Mar 19 Avril 2016 à 10:00
+-- Généré le :  Mar 19 Avril 2016 à 11:08
 -- Version du serveur :  5.5.47-0ubuntu0.14.04.1
 -- Version de PHP :  5.5.9-1ubuntu4.14
 
@@ -20,7 +20,7 @@ SET time_zone = "+00:00";
 -- Base de données :  `JAVA_G4_AF`
 --
 DROP DATABASE IF EXISTS `JAVA_G4_AF`;
-CREATE DATABASE IF NOT EXISTS `JAVA_G4_AF` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+CREATE DATABASE IF NOT EXISTS `JAVA_G4_AF` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `JAVA_G4_AF`;
 
 -- --------------------------------------------------------
@@ -38,7 +38,33 @@ CREATE TABLE IF NOT EXISTS `Quote` (
   `step` int(11) DEFAULT NULL,
   `login` varchar(250) CHARACTER SET utf8 DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+
+--
+-- Vider la table avant d'insérer `Quote`
+--
+
+TRUNCATE TABLE `Quote`;
+--
+-- Contenu de la table `Quote`
+--
+
+INSERT INTO `Quote` (`id`, `name`, `resume`, `price`, `step`, `login`) VALUES
+(1, 'Devis habitation Jean', 'Je suis un résumé du devis de voiture de Jean.', 55, 2, 'Jean@mail.fr'),
+(2, 'Devis voiture Paul', 'Je suis un résumé du devis de voiture de Paul.', 100, 1, 'Paul@mail.fr'),
+(3, 'Devis voiture Claire', 'Je suis un résumé du devis de voiture de Claire.', 515, 0, 'Claire@mail.fr'),
+(4, 'Devis habitation Gertrude', 'Je suis un résumé du devis de voiture de Gertrude.', 60, 4, 'Gertrude@mail.fr'),
+(5, 'Devis habitation Michel', 'Je suis un résumé du devis de voiture de Michel.', 14, 3, 'Michel@mail.fr'),
+(6, 'Devis voiture Louane', 'Je suis un résumé du devis de voiture de Louane.', 23, 2, 'Louane@mail.fr'),
+(7, 'Devis habitation Kendji', 'Je suis un résumé du devis de voiture de Kendji.', 21, 3, 'Kendji@mail.fr'),
+(8, 'Devis voiture Richard', 'Je suis un résumé du devis de voiture de Richard.', 60, 2, 'Richard@mail.fr'),
+(9, 'Devis voiture Arnold', 'Je suis un résumé du devis de voiture de Arnold.', 49, 1, 'Arnold@mail.fr'),
+(10, 'Devis habitation Willy', 'Je suis un résumé du devis de voiture de Willy.', 21, 0, 'Willy@mail.fr'),
+(11, 'Devis voiture Louis', 'Je suis un résumé du devis de voiture de Louis.', 28, 0, 'Louis@mail.fr'),
+(12, 'Devis habitation Carmen', 'Je suis un résumé du devis de voiture de Carmen.', 99, 4, 'Carmen@mail.fr'),
+(13, 'Devis habitation Louisa', 'Je suis un résumé du devis de voiture de Louisa.', 64, 4, 'Louisa@mail.fr'),
+(14, 'Devis voiture Léa', 'Je suis un résumé du devis de voiture de Léa.', 1544, 2, 'Léa@mail.fr'),
+(15, 'Devis voiture Myriam', 'Je suis un résumé du devis de voiture de Myriam.', 78, 1, 'Myriam@mail.fr');
 
 -- --------------------------------------------------------
 
@@ -49,9 +75,9 @@ CREATE TABLE IF NOT EXISTS `Quote` (
 DROP TABLE IF EXISTS `QuoteCar`;
 CREATE TABLE IF NOT EXISTS `QuoteCar` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `brand` varchar(250) CHARACTER SET utf8 DEFAULT NULL,
-  `model` varchar(250) CHARACTER SET utf8 DEFAULT NULL,
-  `fuel` varchar(250) CHARACTER SET utf8 DEFAULT NULL,
+  `brand` int(11) DEFAULT NULL,
+  `model` int(11) DEFAULT NULL,
+  `fuel` int(11) DEFAULT NULL,
   `taxHorsepower` int(11) DEFAULT NULL,
   `dateLicence` date DEFAULT NULL,
   `numberAccident` int(11) DEFAULT NULL,
@@ -65,7 +91,26 @@ CREATE TABLE IF NOT EXISTS `QuoteCar` (
   `idQuote` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_QuoteCar_id` (`idQuote`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+--
+-- Vider la table avant d'insérer `QuoteCar`
+--
+
+TRUNCATE TABLE `QuoteCar`;
+--
+-- Contenu de la table `QuoteCar`
+--
+
+INSERT INTO `QuoteCar` (`id`, `brand`, `model`, `fuel`, `taxHorsepower`, `dateLicence`, `numberAccident`, `bonusMalus`, `garage`, `driver`, `secondaryDriver`, `address`, `allRisk`, `tiers`, `idQuote`) VALUES
+(1, 5, 11, 25, 20, '2012-10-14', 8, 0.5, 0, 'Paul Henry', NULL, NULL, 1, 0, 2),
+(2, 6, 12, 26, 60, '2020-11-04', 5, 0, 1, 'Claire Chazal', 'Gerard Henry', 'Rue des mouffettes', 0, 1, 3),
+(3, 7, 13, 27, 555, '1918-06-11', 2, 1.9, 0, 'Louane Avenir', NULL, NULL, 1, 0, 6),
+(4, 8, 14, 28, 45, '2004-10-14', 1, 1.1, 1, 'Richard Gotener', 'Marie Drucker', 'Rue des papillons bleue', 1, 0, 8),
+(5, 9, 15, 25, 2, '2016-02-16', 0, 0, 0, 'Arnold Schwarzeneger', NULL, NULL, 0, 1, 9),
+(6, 5, 16, 26, 600, '2008-09-02', 0, 1, 1, 'Louis LaBrocante', 'Marcel Letroc', 'Rue des abeilles', 1, 0, 11),
+(7, 6, 17, 27, 84, '2001-01-14', 3, 5, 0, 'Léa Drucker', 'Papa Drucker', NULL, 1, 0, 14),
+(8, 7, 18, 28, 13, '1950-10-19', 6, 2, 1, 'Myriam Ladame', NULL, 'Rue des mouettes qui font caca', 0, 1, 15);
 
 -- --------------------------------------------------------
 
@@ -90,7 +135,25 @@ CREATE TABLE IF NOT EXISTS `QuoteHome` (
   `idQuote` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_QuoteHome_id` (`idQuote`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+--
+-- Vider la table avant d'insérer `QuoteHome`
+--
+
+TRUNCATE TABLE `QuoteHome`;
+--
+-- Contenu de la table `QuoteHome`
+--
+
+INSERT INTO `QuoteHome` (`id`, `typeHome`, `address`, `area`, `numberRoom`, `floor`, `numberBathroom`, `garage`, `groundArea`, `terraceArea`, `typeHeating`, `formula`, `idQuote`) VALUES
+(1, 'Appartement', '29 rue du potager', 75, 3, 2, 3, 1, NULL, NULL, 'Fioul', 'Formule 2', 1),
+(2, 'Studio', '34 rue du marchand de sable', 15, 3, NULL, 3, 0, NULL, NULL, 'Gaz', 'Formule 1', 4),
+(3, 'Maison', '1 rue de la rue', 125, 3, 6, 3, 0, 25, 10, 'Electrique', 'Formule 2', 5),
+(4, 'Studio', '10 avenue de la patate', 25, 3, NULL, 3, 1, NULL, NULL, 'Gaz', 'Formule 1', 7),
+(5, 'Appartement', '9 route des carottes', 49, 3, 6, 3, 0, NULL, NULL, 'Fioul', 'Formule 2', 10),
+(6, 'Maison', '100 boulevard des capucines', 90, 3, NULL, 3, 1, NULL, 40, 'Electrique', 'Formule 1', 12),
+(7, 'Maison', '69 avenue des chiots', 135, 3, NULL, 3, 1, 38, NULL, 'Bois', 'Formule 2', 13);
 
 --
 -- Contraintes pour les tables exportées
@@ -128,7 +191,50 @@ CREATE TABLE IF NOT EXISTS `list` (
   `parent_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `list_parent_id` (`parent_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8;
+
+--
+-- Vider la table avant d'insérer `list`
+--
+
+TRUNCATE TABLE `list`;
+--
+-- Contenu de la table `list`
+--
+
+INSERT INTO `list` (`id`, `listKey`, `value`, `parent_id`) VALUES
+(5, 'MAR', 'Peugeot', NULL),
+(6, 'MAR', 'Ford', NULL),
+(7, 'MAR', 'Porsche', NULL),
+(8, 'MAR', 'Toyota', NULL),
+(9, 'MAR', 'Dacia', NULL),
+(10, 'MAR', 'Kia', NULL),
+(11, 'VEH', 'Clio', 4),
+(12, 'VEH', 'Megane', 4),
+(13, 'VEH', 'Captur', 4),
+(14, 'VEH', '206', 5),
+(15, 'VEH', '3008', 5),
+(16, 'VEH', 'Focus', 6),
+(17, 'VEH', 'Fiesta', 6),
+(18, 'VEH', 'Cayenne', 7),
+(19, 'VEH', '911', 7),
+(20, 'VEH', 'Yaris', 8),
+(21, 'VEH', 'Prius', 8),
+(22, 'VEH', 'Sandero', 9),
+(23, 'VEH', 'Duster', 9),
+(24, 'VEH', 'Rio', 10),
+(25, 'FUE', 'GPL', NULL),
+(26, 'FUE', 'Essence', NULL),
+(27, 'FUE', 'Electrique', NULL),
+(28, 'FUE', 'Hybrid', NULL),
+(31, 'HAB', 'Appartement', NULL),
+(32, 'HAB', 'Maison', NULL),
+(33, 'HAB', 'Studio', NULL),
+(34, 'MAR', 'Renault', NULL),
+(35, 'HEA', 'Electrique', NULL),
+(36, 'HEA', 'Fioul', NULL),
+(37, 'HEA', 'Gaz', NULL),
+(38, 'HEA', 'Bois', NULL);
 
 -- --------------------------------------------------------
 
@@ -144,7 +250,26 @@ CREATE TABLE IF NOT EXISTS `quote` (
   `guarantee` varchar(255) DEFAULT NULL,
   `price` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+--
+-- Vider la table avant d'insérer `quote`
+--
+
+TRUNCATE TABLE `quote`;
+--
+-- Contenu de la table `quote`
+--
+
+INSERT INTO `quote` (`id`, `userId`, `typeQuote`, `guarantee`, `price`) VALUES
+(2, 3, 'habitation', 'maybe', 10),
+(3, 5, 'voiture', 'yes', 1000),
+(4, 6, 'habitation', 'yes', 1012),
+(5, 7, 'voiture', 'maybe', 19),
+(6, 8, 'voiture', 'maybe', 950),
+(7, 8, 'habitation', 'no', 98),
+(8, 9, 'voiture', 'maybe', 54),
+(9, 10, 'habitation', 'no', 63);
 
 -- --------------------------------------------------------
 
@@ -164,48 +289,16 @@ CREATE TABLE IF NOT EXISTS `user` (
   `updatedAt` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `roles` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- Vider la table avant d'insérer `user`
+--
 
-USE JAVA_G4_AF;
-
-INSERT INTO `Quote`(`id`,`name`, `resume`, `price`, `step`, `login`) VALUES (1,"Devis habitation Jean","Je suis un résumé du devis de voiture de Jean.",55,2, "Jean@mail.fr");
-INSERT INTO `Quote`(`id`,`name`, `resume`, `price`, `step`, `login`) VALUES (2,"Devis voiture Paul","Je suis un résumé du devis de voiture de Paul.",100,1, "Paul@mail.fr");
-INSERT INTO `Quote`(`id`,`name`, `resume`, `price`, `step`, `login`) VALUES (3,"Devis voiture Claire","Je suis un résumé du devis de voiture de Claire.",515,0, "Claire@mail.fr");
-INSERT INTO `Quote`(`id`,`name`, `resume`, `price`, `step`, `login`) VALUES (4,"Devis habitation Gertrude","Je suis un résumé du devis de voiture de Gertrude.",60,4, "Gertrude@mail.fr");
-INSERT INTO `Quote`(`id`,`name`, `resume`, `price`, `step`, `login`) VALUES (5,"Devis habitation Michel","Je suis un résumé du devis de voiture de Michel.",14,3, "Michel@mail.fr");
-INSERT INTO `Quote`(`id`,`name`, `resume`, `price`, `step`, `login`) VALUES (6,"Devis voiture Louane","Je suis un résumé du devis de voiture de Louane.",23,2, "Louane@mail.fr");
-INSERT INTO `Quote`(`id`,`name`, `resume`, `price`, `step`, `login`) VALUES (7,"Devis habitation Kendji","Je suis un résumé du devis de voiture de Kendji.",21,3, "Kendji@mail.fr");
-INSERT INTO `Quote`(`id`,`name`, `resume`, `price`, `step`, `login`) VALUES (8,"Devis voiture Richard","Je suis un résumé du devis de voiture de Richard.",60,2, "Richard@mail.fr");
-INSERT INTO `Quote`(`id`,`name`, `resume`, `price`, `step`, `login`) VALUES (9,"Devis voiture Arnold","Je suis un résumé du devis de voiture de Arnold.",49,1, "Arnold@mail.fr");
-INSERT INTO `Quote`(`id`,`name`, `resume`, `price`, `step`, `login`) VALUES (10,"Devis habitation Willy","Je suis un résumé du devis de voiture de Willy.",21,0, "Willy@mail.fr");
-INSERT INTO `Quote`(`id`,`name`, `resume`, `price`, `step`, `login`) VALUES (11,"Devis voiture Louis","Je suis un résumé du devis de voiture de Louis.",28,0, "Louis@mail.fr");
-INSERT INTO `Quote`(`id`,`name`, `resume`, `price`, `step`, `login`) VALUES (12,"Devis habitation Carmen","Je suis un résumé du devis de voiture de Carmen.",99,4, "Carmen@mail.fr");
-INSERT INTO `Quote`(`id`,`name`, `resume`, `price`, `step`, `login`) VALUES (13,"Devis habitation Louisa","Je suis un résumé du devis de voiture de Louisa.",64,4, "Louisa@mail.fr");
-INSERT INTO `Quote`(`id`,`name`, `resume`, `price`, `step`, `login`) VALUES (14,"Devis voiture Léa","Je suis un résumé du devis de voiture de Léa.",1544,2, "Léa@mail.fr");
-INSERT INTO `Quote`(`id`,`name`, `resume`, `price`, `step`, `login`) VALUES (15,"Devis voiture Myriam","Je suis un résumé du devis de voiture de Myriam.",78,1, "Myriam@mail.fr");
-
-INSERT INTO `QuoteCar`(`brand`, `model`, `fuel`, `taxHorsepower`, `dateLicence`, `numberAccident`, `bonusMalus`, `garage`, `driver`, `allRisk`, `tiers`, `idQuote`) VALUES ("Peugeot","206","Diesel",20,"12/10/14", 8,0.5,0,"Paul Henry",1,0,2);
-INSERT INTO `QuoteCar`(`brand`, `model`, `fuel`, `taxHorsepower`, `dateLicence`, `numberAccident`, `bonusMalus`, `garage`, `driver`, `secondaryDriver`, `address`, `allRisk`, `tiers`, `idQuote`) VALUES ("Renault","Megane","Essence",60,"20/11/04", 5,0,1,"Claire Chazal","Gerard Henry","Rue des mouffettes",0,1,3);
-INSERT INTO `QuoteCar`(`brand`, `model`, `fuel`, `taxHorsepower`, `dateLicence`, `numberAccident`, `bonusMalus`, `garage`, `driver`, `allRisk`, `tiers`, `idQuote`) VALUES ("Ford","Fiesta","GPL",555,"1918/06/11", 2,1.9,0,"Louane Avenir",1,0,6);
-INSERT INTO `QuoteCar`(`brand`, `model`, `fuel`, `taxHorsepower`, `dateLicence`, `numberAccident`, `bonusMalus`, `garage`, `driver`, `secondaryDriver`, `address`, `allRisk`, `tiers`, `idQuote`) VALUES ("Toyota","Yaris","Electrique",45,"2004/10/14", 1,1.1,1,"Richard Gotener","Marie Drucker","Rue des papillons bleue",1,0,8);
-INSERT INTO `QuoteCar`(`brand`, `model`, `fuel`, `taxHorsepower`, `dateLicence`, `numberAccident`, `bonusMalus`, `garage`, `driver`, `allRisk`, `tiers`, `idQuote`) VALUES ("Porshe","911","Essence",2,"2016/02/16", 0,0,0,"Arnold Schwarzeneger",0,1,9);
-INSERT INTO `QuoteCar`(`brand`, `model`, `fuel`, `taxHorsepower`, `dateLicence`, `numberAccident`, `bonusMalus`, `garage`, `driver`, `secondaryDriver`, `address`, `allRisk`, `tiers`, `idQuote`) VALUES ("Mercedes","A","Diesel",600,"08/09/02", 0,1,1,"Louis LaBrocante","Marcel Letroc","Rue des abeilles",1,0,11);
-INSERT INTO `QuoteCar`(`brand`, `model`, `fuel`, `taxHorsepower`, `dateLicence`, `numberAccident`, `bonusMalus`, `garage`, `driver`, `secondaryDriver`, `allRisk`, `tiers`, `idQuote`) VALUES ("Peugeot","208","GPL",84,"2001/01/14", 3,5,0,"Léa Drucker","Papa Drucker",1,0,14);
-INSERT INTO `QuoteCar`(`brand`, `model`, `fuel`, `taxHorsepower`, `dateLicence`, `numberAccident`, `bonusMalus`, `garage`, `driver`, `address`, `allRisk`, `tiers`, `idQuote`) VALUES ("Renault","Captur","Diesel",13,"1950/10/19", 6,2,1,"Myriam Ladame","Rue des mouettes qui font caca",0,1,15);
-
-INSERT INTO `QuoteHome`(`typeHome`, `address`, `area`, `numberRoom`, `floor`, `numberBathroom`, `garage`,`typeHeating`, `formula`, `idQuote`) VALUES ("Appartement","29 rue du potager",75,3,2,3,1,"Fioul","Formule 2",1);
-INSERT INTO `QuoteHome`(`typeHome`, `address`, `area`, `numberRoom`, `numberBathroom`, `garage`, `typeHeating`, `formula`, `idQuote`) VALUES ("Studio","34 rue du marchand de sable",15,3,3,0,"Gaz","Formule 1",4);
-INSERT INTO `QuoteHome`(`typeHome`, `address`, `area`, `numberRoom`, `floor`, `numberBathroom`, `garage`, `groundArea`, `terraceArea`, `typeHeating`, `formula`, `idQuote`) VALUES ("Maison","1 rue de la rue",125,3,6,3,0,25,10,"Electrique","Formule 2",5);
-INSERT INTO `QuoteHome`(`typeHome`, `address`, `area`, `numberRoom`, `numberBathroom`, `garage`,`typeHeating`, `formula`, `idQuote`) VALUES ("Studio","10 avenue de la patate",25,3,3,1,"Gaz","Formule 1",7);
-INSERT INTO `QuoteHome`(`typeHome`, `address`, `area`, `numberRoom`, `floor`, `numberBathroom`, `garage`, `typeHeating`, `formula`, `idQuote`) VALUES ("Appartement","9 route des carottes",49,3,6,3,0,"Fioul","Formule 2",10);
-INSERT INTO `QuoteHome`(`typeHome`, `address`, `area`, `numberRoom`, `numberBathroom`, `garage`, `terraceArea`, `typeHeating`, `formula`, `idQuote`) VALUES ("Maison","100 boulevard des capucines",90,3,3,1,40,"Electrique","Formule 1",12);
-INSERT INTO `QuoteHome`(`typeHome`, `address`, `area`, `numberRoom`, `numberBathroom`, `garage`, `groundArea`, `typeHeating`, `formula`, `idQuote`) VALUES ("Maison","69 avenue des chiots",135,3,3,1,38,"Bois","Formule 2",13);
-
-USE JAVA_G4_MS;
+TRUNCATE TABLE `user`;
+--
+-- Contenu de la table `user`
+--
 
 INSERT INTO `user` (`id`, `name`, `surname`, `login`, `password`, `active`, `createdAt`, `updatedAt`, `roles`) VALUES
 (2, 'Wayne', 'Bruce', 'batman', '123456', 1, '2012-02-22', '2016-03-22 00:00:00', 'ROLE_ADMIN'),
@@ -218,50 +311,6 @@ INSERT INTO `user` (`id`, `name`, `surname`, `login`, `password`, `active`, `cre
 (9, 'Prince', 'Diana', 'wonderwoman', '123456', 0, '2008-03-22', '2016-03-22 00:00:00', 'ROLE_USER'),
 (10, 'Allen', 'Barry', 'flash', '123456', 1, '2015-11-22', '2016-03-22 00:00:00', 'ROLE_USER');
 
-
-INSERT INTO `quote` (`id`, `userId`, `typeQuote`, `guarantee`, `price`) VALUES
-(2, 3, 'habitation', 'maybe', 10),
-(3, 5, 'voiture', 'yes', 1000),
-(4, 6, 'habitation', 'yes', 1012),
-(5, 7, 'voiture', 'maybe', 19),
-(6, 8, 'voiture', 'maybe', 950),
-(7, 8, 'habitation', 'no', 98),
-(8, 9, 'voiture', 'maybe', 54),
-(9, 10, 'habitation', 'no', 63);
-
-
-INSERT INTO `list` (`id`, `listKey`, `value`, `parent_id`) VALUES
-(34, 'MAR', 'Renault', null),
-(5, 'MAR', 'Peugeot', null),
-(6, 'MAR', 'Ford', null),
-(7, 'MAR', 'Porsche', null),
-(8, 'MAR', 'Toyota', null),
-(9, 'MAR', 'Dacia', null),
-(10, 'MAR', 'Kia', null),
-(11, 'MOD', 'Clio', 4),
-(12, 'MOD', 'Megane', 4),
-(13, 'MOD', 'Captur', 4),
-(14, 'MOD', '206', 5),
-(15, 'MOD', '3008', 5),
-(16, 'MOD', 'Focus', 6),
-(17, 'MOD', 'Fiesta', 6),
-(18, 'MOD', 'Cayenne', 7),
-(19, 'MOD', '911', 7),
-(20, 'MOD', 'Yaris', 8),
-(21, 'MOD', 'Prius', 8),
-(22, 'MOD', 'Sandero', 9),
-(23, 'MOD', 'Duster', 9),
-(24, 'MOD', 'Rio', 10),
-(25, 'FUE', 'GPL', null),
-(26, 'FUE', 'Essence', null),
-(27, 'FUE', 'Electrique', null),
-(28, 'FUE', 'Hybrid', null),
-(29, 'FOR', 'Formule 1', null),
-(30, 'FOR', 'Formule 2', null),
-(31, 'HAB', 'Appartement', null),
-(32, 'HAB', 'Maison', null),
-(33, 'HAB', 'Studio', null),
-(35, 'HEA', 'Electrique', null),
-(36, 'HEA', 'Fioul', null),
-(37, 'HEA', 'Gaz', null),
-(38, 'HEA', 'Bois', null);
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
